d2efd8f443d1f17a9f87a04657419e0047276500
goldenpants1 <grog.88@mail.ru>
2024-12-28 01:43:40 +0300

    Задание урока 3.6 шага 10 для ревью. Проверка наличия кнопки добавления в корзину

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default= "chrome",
+                     help="Choose browser: chrome or firefox")
+
+    parser.addoption('--language', action='store', default=None, help="Choose language: '--language=en' or '--language=ru'")
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    #browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nstart chrome browser for test...")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", user_language)
+        print("\nstart firefox browser for test...")
+        browser = webdriver.Firefox(options=options_firefox)
+    yield browser
+    print("\nquit browser...")
+    browser.quit()
