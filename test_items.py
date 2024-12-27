2ebe97079e7c88b68b1b53fc819ee35248707ca6
goldenpants1 <grog.88@mail.ru>
2024-12-28 01:55:48 +0300

    До заливаю файлы после урока 3.6, шага 10 для ревью

--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_have_button_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
-    buttonBasket = browser.find_elements(By.CSS_SELECTOR, "form#add_to_basket_form>button")
+    buttonBasket = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form>button")
     assert len(buttonBasket) > 0, "Кнопка добавления в корзину не найдена..."
 
 
